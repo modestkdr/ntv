79b8aad2811d071fd5e61d8566e09a970131b1c7
srikanth <sri@ubuntu.ubuntu-domain>
2012-05-17 14:07:55 -0400

    added lot of modules

---
+++ mockups/accordion/accordion-horizontal-auto/lib/jquery.hrzAccordion.examples.js
@@ -0,0 +1,14 @@
+
+$(document).ready(function() {
+$(".test2").hrzAccordion({
+	handlePosition     :"right",
+	openOnLoad     :5,
+	closeOpenAnimation: 1,
+	cycle: true,
+	eventTrigger:"click",
+    pauseOnHover : true,
+	
+							  }); 	
+ });
+ 
+ 
